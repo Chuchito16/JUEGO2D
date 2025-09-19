c33811ca7f8f1a317376a2cb45f9655eb8a3cde7
Chuchito16 <142456464+Chuchito16@users.noreply.github.com>
2025-09-19 17:25:21 -0500

    Record timer time when changing scenes

--- Cg_2d_game/Assets/Scripts/LoaderScenes.cs
+++ Cg_2d_game/Assets/Scripts/LoaderScenes.cs
@@ -3,20 +3,65 @@ using UnityEngine.SceneManagement;
 
 public class LoaderScenes : MonoBehaviour
 {
+    [SerializeField]
+    private Timer activeTimer;
+
+    private void Awake()
+    {
+        EnsureTimerReference();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void lectorEscena(string nameScene)
     {
-        //añadir aca un metodo para devolver el valor del tiempo que llevamos y lo devolvemos al timer
+        EnsureTimerReference();
+        if (activeTimer != null)
+        {
+            activeTimer.TimerStop();
+
+            float currentTotal = activeTimer.CurrentTime;
+            GameManager manager = GameManager.instance;
+            if (manager != null)
+            {
+                float previousTotal = manager.GlobalTime1;
+                float delta = currentTotal - previousTotal;
+                if (delta < 0f)
+                {
+                    delta = currentTotal;
+                }
+                manager.SumaTimeGlobal(delta);
+            }
+            else
+            {
+                Debug.LogWarning("LoaderScenes: GameManager instance not found when recording time.");
+            }
+
+            activeTimer.TimerReset();
+            activeTimer = null;
+        }
+        else
+        {
+            Debug.LogWarning("LoaderScenes: No Timer found to record time before loading scene.");
+        }
+
         SceneManager.LoadScene(nameScene);
+    }
+
+    private void EnsureTimerReference()
+    {
+        if (activeTimer == null)
+        {
+            activeTimer = FindObjectOfType<Timer>();
+        }
     }
 }
